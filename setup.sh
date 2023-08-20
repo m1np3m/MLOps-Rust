fa48e7e1f109563cdfde3faa9f8a38ad35d00892
manpham <KendrickPh4m@gmail.com>
2023-08-20 08:36:11 +0000

    update code

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+# git config
+git config --global user.name "m1np3m"
+git config --global user.email "KendrickPh4m@gmail.com"
+
 #some setup stuff for the dev environment
 #install nodejs
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 
 #install GitHub Copilot CLI
-npm install -g @githubnext/github-copilot-cli
+sudo npm install -g @githubnext/github-copilot-cli
 
 #authenticate with GitHub Copilot
 github-copilot-cli auth
 
-#Upgrade
-#npm install -g @githubnext/github-copilot-cli
